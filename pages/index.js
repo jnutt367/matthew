ae654a8fa3ea438a62c45a0a033bcb00ee20653a
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-08 11:24:35 -0500

    Update index.js
    book-matthew.jpg

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
         </h1>
       <div className={styles.hero}>
        <Image 
-                                src="/matthew.jfif"
+                                src="/book-matthew.jpg"
                                 alt="Jesus teaching"
                                 height={900}
                                 width={2000}
