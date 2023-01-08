0ddcb6397dbe56f00697842e0e5c9c0ab777142c
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-08 10:30:45 -0500

    Update index.js
    book-matthew.jpg correctly added

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
         </h1>
       <div className={styles.hero}>
        <Image 
-                                src="/book_matthew.jpg"
+                                src="/book-matthew.jpg"
                                 alt="Jesus teaching"
                                 height={900}
                                 width={2000}
