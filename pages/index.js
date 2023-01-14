06c857fc212f59e504fb0779c6f36a92a8f0461c
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-13 22:28:55 -0500

    Update index.js
    Matthew_1.jpg

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
         </h1>
       <div className={styles.hero}>
        <Image 
-                                src="/matthew.webp"
+                                src="/Matthew_1.jpg"
                                 alt="Jesus teaching"
                                 height={900}
                                 width={2000}
