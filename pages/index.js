223fbae2b5603feaba65e5e10556f9f6264b7373
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-28 05:43:58 -0500

    Update index.js
     className={styles.image} height 500px

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
        <Image
                                 src="/istockphoto-1156403463-612x612.jpg"
                                 alt="Proverbs Mug wide view"
-                                height={900}
+                                height={500}
                                 width={2000}
                                 priority
                               />
