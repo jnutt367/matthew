9ae3a79cadc703c527ec3ee018938416d06ff94f
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-03-04 01:57:48 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1582,6 +1582,15 @@ The Great Commission
 16 Then the eleven disciples went to Galilee, to the mountain where Jesus had told them to go. 17 When they saw him, they worshiped him; but some doubted. 18 Then Jesus came to them and said, “All authority in heaven and on earth has been given to me. 19 Therefore go and make disciples of all nations, baptizing them in the name of the Father and of the Son and of the Holy Spirit, 20 and teaching them to obey everything I have commanded you. And surely I am with you always, to the very end of the age.”
             </p>
           </div>
+  <Image
+                                src="/istockphoto-1165307675-612x612.jpg"
+                                alt="Jesus carries his cross"
+                                height={900}
+                                width={2000}
+                                priority
+                              />
+        </div>            
+  
         </div>
 </div>
         <p className={styles.description}>
