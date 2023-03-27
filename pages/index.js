b02f43cfd5aa483cbced8ab50acf22f2ba3a91c9
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-03-26 21:29:28 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,15 +18,15 @@ export default function Home() {
       <main className={styles.main}>
      
       <div className={styles.hero}>
-        
-       <Image className={styles.workshop}
+        <div className={styles.workshop}>
+       <Image 
                                 src="/workshop.png"
                                 alt="Jesus birth gif"
                                 height={75}
                                 width={75}
                                 priority
                               />
-                      
+        </div>              
             <h2 className={styles.holy}>THE HOLY</h2>
                 <h1 className={styles.bible}>
                     <a href="https://the-holy-bible.vercel.app/" target="_blank">BIBLE</a>
