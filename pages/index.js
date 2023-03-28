0da22361d724bc32cdef005f87fa0a17cacfa6b0
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-03-27 20:40:36 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import ReactPlayer from 'react-player/lazy'
+import ReactPlayer from 'next/react-player'
 export default function Home() {
   return (
     <div className={styles.container}>
