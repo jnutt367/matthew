37ec0753602e69149038b3dacbce5d574a306530
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-04-18 17:51:17 -0400

    Update index.js
    Praying.fbx

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
          <div  className={styles.image}>
                      <a href="https://matthew-coral.vercel.app/">
                                <Image 
-                                src="/Praying (1).fbx"
+                                src="/Praying.fbx"
                                 alt="Nativity silent night gif"
                                 height={900}
                                 width={2000}
