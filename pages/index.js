f555179c379da5b0c16501a223d285704a87011c
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-04-18 23:41:50 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -48,7 +48,7 @@ export default function Home() {
                 
                             
  
-<iframe width="100%" height="200" src="https://www.youtube.com/embed/J0p9Qp1cyfk" title="YouTube video player" frameborder="5" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>       
+<iframe width="100%" height="250" src="https://www.youtube.com/embed/3-3xRwS4J3Q" title="YouTube video player" frameborder="5" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>       
     </div>         
 <div className={styles.nav}>               
        <a href="https://the-holy-bible.vercel.app/">
