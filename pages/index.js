03deb4fd0b4a0ee0747d8ebf420d3a9db5ce4637
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-04-21 10:33:10 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -45,7 +45,7 @@ export default function Home() {
                                 priority
                               />
                      </a>
-                
+                </div>
                             
  
 
