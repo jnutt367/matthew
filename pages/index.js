2a0717bfeb2ac53c35788394b89f3c9244785f49
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-07-20 16:49:01 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,9 +54,9 @@ export default function Home() {
                               />
        </a>  
   
-  <a href="https://the-word-of-god.vercel.app/">
+  <a href="https://cash.app/$JNutt367">
               <Image className={styles.arrow}
-                                src="/home.webp"
+                                src="/donations_please.webp"
                                 alt="youtube-icon"
                                 height={75}
                                 width={90}
