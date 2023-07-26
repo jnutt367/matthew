2ff18763a2394eedb89fed8b0eebe1ed1c0ea22b
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-07-26 19:50:39 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1589,9 +1589,12 @@ The Great Commission
 16 Then the eleven disciples went to Galilee, to the mountain where Jesus had told them to go. 17 When they saw him, they worshiped him; but some doubted. 18 Then Jesus came to them and said, “All authority in heaven and on earth has been given to me. 19 Therefore go and make disciples of all nations, baptizing them in the name of the Father and of the Son and of the Holy Spirit, 20 and teaching them to obey everything I have commanded you. And surely I am with you always, to the very end of the age.”
             </p>
           </div>
+
+ <div className={styles.iframe}>
+  <iframe width="100%" height="600" src="https://www.youtube.com/embed/GGCF3OPWN14" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>  </div>
+  </div>  
        </div>    
-<iframe width="100%" height="600" src="https://www.youtube.com/embed/GGCF3OPWN14" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>  </div>
-           
+ 
   </main>
 
 <main id="first_timothy" className={styles.main}>
