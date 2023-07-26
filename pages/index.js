b545c638447add60523f5513e503187a121a5c24
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-07-26 19:58:45 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1590,11 +1590,12 @@ The Great Commission
             </p>
           </div>
 
+          
+          </div>  
            <div className={styles.iframe}>
           <iframe width="100%" height="600" src="https://www.youtube.com/embed/GGCF3OPWN14" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>  
   
-            </div>
-          </div>  
+            </div>  
        </div>    
  
   </main>
