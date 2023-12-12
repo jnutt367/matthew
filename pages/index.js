f436641dea7d1c380933e1a368b01cecb60128fe
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-12-12 14:12:06 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
         </h1>
          <div  className={styles.iframe1}>
                    
-                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/QiX51E4TBjI?si=t_CT3fkkqpbAgitG&autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/QiX51E4TBjI?si=t_CT3fkkqpbAgitG&autoplay=1&mute=1" title="YouTube video player" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                             
 </div> 
 <p className={styles.parables}>
@@ -1654,7 +1654,7 @@ Paul wrote to a very young Timothy, in order to instruct on how to live as a "ma
          
                </div>
                <div className={styles.iframe}> 
- <iframe width="100%" height="315" src="https://www.youtube.com/embed/3Dv4-n6OYGI?autoplay=1&mute=1" title="YouTube video player" frameborder="2" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+ <iframe width="100%" height="315" src="https://www.youtube.com/embed/3Dv4-n6OYGI?autoplay=1&mute=1" title="YouTube video player" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                                   </div>
                                   
                               
